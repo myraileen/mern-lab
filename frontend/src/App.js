35d3a5cdf14a1e9731d1293277d685252779ef73
Myra Fah <FahMyra@JohnDeere.com>
2020-03-18 12:50:36 -0500

    added axios dependency. connected frontend react app to backend mongodb

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+const backendUrl = "http://localhost:8080/api/users/";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super()
+  }
+
+  componentDidMount() {
+    axios({method: 'GET', url: backendUrl})
+      .then(users => console.log(users))
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
 }
 
 export default App;
