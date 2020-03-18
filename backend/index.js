8ebe36f6d04a0644f5981eddc438a43672d3117a
Myra Fah <FahMyra@JohnDeere.com>
2020-03-18 10:51:58 -0500

    updated index.js to require CORS

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const usersController = require("./controllers/users");
+const cors = require("cors");
 
 //Middleware
+app.use(cors());
 app.use(express.json());
 
 //Controllers
