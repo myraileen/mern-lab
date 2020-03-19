39c4dc95a6c48d7ad855f414ea8db87cd73604cc
Myra Fah <FahMyra@JohnDeere.com>
2020-03-19 09:41:28 -0500

    verified backend post new to-do with postman

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -3,29 +3,44 @@ const router = express.Router();
 const User = require("../models/User");
 
 // GET ALL USERS
-router.get('/', (req, res) => {
-    User.find().then(users => res.json(users))
-  })	
+router.get("/", (req, res) => {
+  User.find().then(users => res.json(users));
+});
 
 // GET USER BY ID
 router.get("/:id", (req, res) => {
-    User.findById(req.params.id).then(user => res.json(user))
+  User.findById(req.params.id).then(user => res.json(user));
 });
 
 // CREATE A USER
 router.post("/", (req, res) => {
-    User.create(req.body).then(newUser => res.json(newUser))
+  User.create(req.body).then(newUser => res.json(newUser));
 });
 
 // UPDATE A USER
 router.put("/:id", (req, res) => {
-    User.findByIdAndUpdate(req.params.id, req.body, {new: true})
-    .then(updatedUser => res.json(updatedUser))
+  User.findByIdAndUpdate(req.params.id, req.body, {
+    new: true
+  }).then(updatedUser => res.json(updatedUser));
 });
 
 // DELETE A USER
 router.delete("/:id", (req, res) => {
-    User.findByIdAndDelete(req.params.id).then(deletedUser => res.json(deletedUser))
+  User.findByIdAndDelete(req.params.id).then(deletedUser =>
+    res.json(deletedUser)
+  );
+});
+
+// CREATE A NEW TODO
+router.post("/:userId/new-todo/", (req, res) => {
+  User.findById(req.params.userId).then(user => {
+    let newTodo = req.body;
+    newTodo.done = false;
+
+    user.todos.push(newTodo);
+    user.save();
+    res.json(user);
+  });
 });
 
 module.exports = router;
